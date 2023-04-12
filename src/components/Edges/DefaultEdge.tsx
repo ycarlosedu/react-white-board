c2a70888bafe2e4526dff621d5506c57e3e52cde
Carlos Eduardo <silvacarlos02@sicredi.com.br>
2023-04-12 15:49:27 -0300

    feat: action buttons in edge

--- src/components/Edges/DefaultEdge.tsx
+++ src/components/Edges/DefaultEdge.tsx
@@ -1,4 +1,5 @@
-import { EdgeLabelRenderer, EdgeProps, getSimpleBezierPath, getSmoothStepPath, getStraightPath } from "reactflow";
+import { Trash } from "phosphor-react";
+import { Edge, EdgeLabelRenderer, EdgeProps, getSimpleBezierPath, getSmoothStepPath, getStraightPath, useReactFlow } from "reactflow";
 
 const backgroundColors = {
   yellow: "#ffcc00",
@@ -50,6 +51,15 @@ export function DefaultEdge({
     targetPosition,
   });
 
+  const { deleteElements, getEdge } = useReactFlow()
+  const edge = getEdge(id)
+  const foreignObjectSize = 400;
+
+  const onEdgeClick = (evt: any, id: string) => {
+    evt.stopPropagation();
+    deleteElements({edges: [edge!]})
+  };
+
   return (
     <>
       <path
@@ -62,18 +72,35 @@ export function DefaultEdge({
       <EdgeLabelRenderer>
         <div
           style={{
-            position: 'absolute',
             transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
             background: backgroundColors[data?.labelColor as BackgroundColors] as string || backgroundColors.transparent,
             color: fontColors[data?.fontColor as FontColors] as string || fontColors.white,
-            padding: 10,
-            borderRadius: 5,
-            fontSize: 12,
-            fontWeight: 700,
           }}
-          className="nodrag nopan"
+          className="nodrag nopan pointer-events-auto p-3 rounded font-bold text-xs absolute"
         >
-          {data?.label}
+          <input type="text" value={data?.label} className="nopan pointer-events-auto bg-transparent outline-none text-center" />
+
+          {selected && (
+            <foreignObject
+              width={foreignObjectSize}
+              height={foreignObjectSize}
+              // x={labelX - (foreignObjectSize / 2) - 50}
+              x={labelX - foreignObjectSize / 2}
+              y={labelY - foreignObjectSize / 2}
+              requiredExtensions="http://www.w3.org/1999/xhtml"
+            >
+              <div
+                className="bg-white rounded-2xl shadow-lg border border-zinc-300 px-2 gap-2 left-1/2 -translate-x-1/2 h-6 flex items-center justify-center absolute -top-10"
+              >
+                <button
+                  className="nopan pointer-events-auto border-x border-zinc-300 w-5 h-full flex items-center justify-center text-xs text-black transition-all hover:bg-zinc-200"
+                  onClick={(event) => onEdgeClick(event, id)}
+                >
+                  <Trash size={16} color="#cb1818" />
+                </button>
+              </div>
+            </foreignObject>
+          )}
         </div>
       </EdgeLabelRenderer>
     </>
